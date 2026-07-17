2f485dad91c65837efbc887e196f437f17f8d617
RPG-0704 <vardhanmane11@gmail.com>
2026-07-17 10:47:42 +0530

    Add files via upload

---
+++ operations.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1 =7;
+int num2 =10;
+cout<<"Addition="<< num1+num2<<endl;
+cout<<"Substraction="<< num1-num2<<endl;
+cout<<"Multiplication="<<num1*num2<<endl;
+cout<<"Modulus="<<num1%num2<<endl;
+}
---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+int b;
+int sum=a+b;
+cout<<"sum";
+}
